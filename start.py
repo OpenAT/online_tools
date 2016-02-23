31bfef2d67c4f7ffbdc3a8f83a24f545e3410a58
Michael Karrer <michaelkarrer81@gmail.com>
2016-02-23 20:24:22 +0100

    [FIX]

--- start.py
+++ start.py
@@ -592,23 +592,23 @@ def _odoo_restore(backup_dir, conf, data_dir_target='', database_target_url=''):
     try:
         datname = "'%s'" % database_name
         # Drop Connections
-        sql_drop_conn = '''"SELECT pg_terminate_backend(pid) \
-                           FROM pg_stat_activity \
-                           WHERE pg_stat_activity.datname = %s \
-                           AND pid <> pg_backend_pid() ;"''' % datname
-        cmd_drop_conn = ['psql', '-q', '--command='+sql_drop_conn, '--dbname='+database_target_url, ]
+        sql_drop_conn = "SELECT pg_terminate_backend(pid) \
+                         FROM pg_stat_activity \
+                         WHERE pg_stat_activity.datname = %s \
+                         AND pid <> pg_backend_pid() ;" % datname
+        cmd_drop_conn = ['psql', '-q', '-c', sql_drop_conn, '-d', database_target_url]
         # Drop DB
-        sql_drop_db = '''"DROP DATABASE %s ;"''' % datname
-        cmd_drop_db = ['psql', '-q', '--command='+sql_drop_db, '--dbname='+database_target_url, ]
+        sql_drop_db = "DROP DATABASE %s ;" % datname
+        cmd_drop_db = ['psql', '-q', '-c', sql_drop_db, '-d', database_target_url]
         # Create DB
-        sql_create_db = '''"CREATE DATABASE %s \
-                           WITH OWNER %s \
-                           TEMPLATE template0 \
-                           ENCODING 'UTF8' \
-                           LC_COLLATE 'de_DE.UTF8' \
-                           LC_CTYPE 'de_DE.UTF8' ;"''' % (database_name, conf['instance'])
+        sql_create_db = "CREATE DATABASE %s \
+                         WITH OWNER %s \
+                         TEMPLATE template0 \
+                         ENCODING 'UTF8' \
+                         LC_COLLATE 'de_DE.UTF8' \
+                         LC_CTYPE 'de_DE.UTF8' ;" % (database_name, conf['instance'])
         sql_create_db_url = database_target_url.rsplit('/', 1)[-2] + 'postgres'
-        cmd_create_db = ['psql', '-q', '--command='+sql_create_db, '--dbname='+sql_create_db_url, ]
+        cmd_create_db = ['psql', '-q', '-c', sql_create_db, '-d', sql_create_db_url]
         with open(os.devnull, 'w') as devnull:
             shell(cmd_drop_conn, timeout=240, stderr=devnull)
             shell(cmd_drop_db, timeout=240, stderr=devnull)
