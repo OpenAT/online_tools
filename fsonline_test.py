263cd32569e695d0454a1a35d592cbffebdb3e7c
Michael Karrer <michaelkarrer81@gmail.com>
2019-12-02 19:19:30 +0100

    test

--- fsonline_test.py
+++ fsonline_test.py
@@ -1,27 +1,4 @@
 #!/usr/bin/env python
-"""
-This script can start, backup, restore or update FS-Online instances on production or development machines.
-
-Order of Actions if multiple are given: Backup -> Restore -> Update -> Start
-
-Conventions:
-  - Instance folder name = Database name = Instance name = Linux user name (on prod machines)
-  - server.conf is directly located in the instance directory (or missing on dev machines)
-  - All paths are relative to the instance directory
-  - All submodules are linked via ssh e.g.: git@github.com:OpenAT/fundraising_studio.git
-  - online repository is either directly inside the instance e.g.:
-    /opt/instances/[instance]/fundraising_studio/online
-    or at the same level as the instance in folders like online_[SHA1] e.g.:
-    /opt/instances/[instance]
-    /opt/instances/online_88b145319056b9d6feac9c86458371cd12a3960c
-  - Full online repo for pull and copy is located at the same level as the instance and called online e.g.:
-    /opt/instances/online
-  - online_tools repo is at the same level as the instances e.g.:
-    /opt/instances/online_tools
-  - status.ini was removed - all relevant info is logged to the update log file
-  - if an update or restore fails a file called update_restore_failed is created -
-    no further updates or restore will run by this script as long as this file exits except --force is set
-"""
 import argparse
 import os
 from os.path import join as pj
@@ -35,6 +12,7 @@ from backup import backup
 from restore import restore
 from update import update
 from tools_settings import Settings
+
 
 import logging
 #import logging.handlers
@@ -57,45 +35,6 @@ log_sys_handler.setFormatter(log_formatter)
 log_sys_handler.setLevel(logging.DEBUG)
 # Add the handler to the root logger
 _log.addHandler(log_sys_handler)
-
-
-# def excepthook(*eargs):
-#     # Get the root logger and log to CRITICAL
-#     logging.getLogger(__name__).critical('Uncaught exception:\n'
-#                                          '-------------------\n', exc_info=eargs)
-#
-#
-# # Redirect sys assertion outputs to the logger
-# sys.excepthook = excepthook
-
-
-# ------------------------------------
-# COMMAND PARSER DEFAULT FUNCTION CALL
-# ------------------------------------
-def fs_online():
-    # BACKUP
-    if known_args.backup:
-        result = backup(known_args.instance_dir, backup_file=known_args.backup,
-                        cmd_args=unknown_args, log_file=known_args.log_file)
-        if result:
-            exit(0)
-        else:
-            exit(100)
-
-    # RESTORE
-    if known_args.restore:
-        restore(known_args.instance_dir, backup_zip_file=known_args.restore,
-                cmd_args=unknown_args, log_file=known_args.log_file)
-        exit(0)
-
-    # UPDATE
-    if known_args.update:
-        update(known_args.instance_dir, cmd_args=unknown_args, log_file=known_args.log_file)
-        exit(0)
-
-    # START
-    return start(known_args.instance_dir, cmd_args=unknown_args, log_file=known_args.log_file)
-
 
 # ----------------------------
 # COMMAND PARSER
@@ -133,7 +72,7 @@ parser.add_argument('--update',
                     help='Update the instance to latest commit of the instance repository on github.')
 
 # Set a default function to be called after the initialization of the parser object
-parser.set_defaults(func=fs_online)
+#parser.set_defaults(func=fs_online)
 
 
 # --------------------
@@ -204,8 +143,8 @@ if __name__ == "__main__":
     _log.info('sys.executable: %s' % str(sys.executable))
 
     # Load configuration
-    _log.info("Prepare instance settings")
-    s = Settings(instance_dir, startup_args=cmd_args, log_file=log_file)
+    # _log.info("Prepare instance settings")
+    # s = Settings(instance_dir, startup_args=cmd_args, log_file=log_file)
 
     # Prepare the instance core
     # _log.info("Prepare the odoo core %s" % s.instance_core_tag)
@@ -213,28 +152,27 @@ if __name__ == "__main__":
     #              production_server=s.production_server)
 
     # Change current working directory to the folder odoo_dir inside the repo online
-    working_dir = pj(s.instance_core_dir, 'odoo')
+    working_dir = pj('/opt/online/cores/online_o8r321', 'odoo')
     sys.path[0] = sys.argv[0] = working_dir
-    _log.info("Change working directory to 'odoo' folder of core dir %s" % working_dir)
+    _log.info("Change working directory to 'odoo' folder of core dir: %s" % working_dir)
     os.chdir(working_dir)
-
-    # Change the current python script working directory to folder odoo_dir inside the repo online
-    _log.info("Set python working directory (sys.path[0] and sys.argv[0]) to 'odoo' folder %s" % working_dir)
     assert working_dir == os.getcwd() == sys.path[0], (
             'Could not change working directory to %s !' % working_dir)
 
     # Overwrite the original script cmd args with the odoo-only ones
     #sys.argv = sys.argv[0:1] + s.startup_args
+    sys.argv = sys.argv[0:1] + sys.argv[2:]
 
     # _log basic info
     _log.info('sys.argv: %s' % str(sys.argv))
     _log.info('sys.executable: %s' % str(sys.executable))
-    _log.info('Production Server: %s' % s.production_server)
-    _log.info('Instance: %s' % s.instance)
-    _log.info('Instance core tag: %s' % s.instance_core_tag)
-    _log.info('Instance core dir: %s' % s.instance_core_dir)
-    _log.info('Instance data_dir: %s' % s.data_dir)
-    _log.info('Instance addon_path: %s' % s.addons_path)
+
+    # _log.info('Production Server: %s' % s.production_server)
+    # _log.info('Instance: %s' % s.instance)
+    # _log.info('Instance core tag: %s' % s.instance_core_tag)
+    # _log.info('Instance core dir: %s' % s.instance_core_dir)
+    # _log.info('Instance data_dir: %s' % s.data_dir)
+    # _log.info('Instance addon_path: %s' % s.addons_path)
 
     # _log system environment information
     _log.info("Environment $PATH: %s" % os.getcwd())
