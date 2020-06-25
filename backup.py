f27b903b465a3fb21680ab73611c910d1a47cf2e
Michael Karrer <michaelkarrer81@gmail.com>
2020-06-25 17:04:44 +0200

    [IMP] change argparser to support subcommands [IMP] add new manual backup mode for backups on other host than the instance host

--- backup.py
+++ backup.py
@@ -10,77 +10,126 @@ import logging
 _log = logging.getLogger()
 
 
-def backup(instance_dir, backup_file='', mode='manual', log_file='', cmd_args=None, settings=None, timeout=60*60*4):
+class BackupSettings:
+    def __init__(self, instance_dir, env=None, backup_file=None, mode=None, db_url=None, data_dir=None, cmd_args=None,
+                 settings=None, log_file=None, ):
+
+        # Pass-Through variables
+        self.instance_dir = instance_dir
+        self.backup_file = backup_file
+
+        # Computed variables
+        self.odoo_db_name = None
+        self.odoo_db_url = None
+        self.odoo_host = None
+        self.odoo_data_dir = None
+        self.odoo_master_pwd = None
+
+        # LOW LEVEL BACKUP WITHOUT ODOO / ODOO SETTINGS
+        if db_url or data_dir:
+            assert not settings, "settings must be empty when db_url or data_dir is given"
+            assert backup_file != 'default', "backup_file missing"
+            backup_dir = os.path.dirname(backup_file)
+            assert os.access(backup_dir, os.R_OK), "Backup directory not writeable at %s" % backup_dir
+            assert mode == 'manual', "mode must be 'manual' if db_url or data_dir is given!"
+            assert db_url, "db_url missing"
+            assert os.path.exists(data_dir), "data_dir not found at %s" % data_dir
+
+            self.odoo_db_name = db_url.rsplit('/', 1)[1]
+            self.odoo_db_url = db_url
+            self.odoo_host = None
+            self.odoo_data_dir = data_dir
+            self.odoo_master_pwd = None
+
+        # BACKUP WITH ODOO SETTINGS ON ODOO SERVICE HOST
+        else:
+            # Cleanup for instance_dir
+            self.instance_dir = os.path.abspath(instance_dir)
+
+            # Get odoo settings
+            if not settings:
+                _log.info("Get instance settings from %s" % instance_dir)
+                s = Settings(instance_dir, startup_args=cmd_args, log_file=log_file)
+            else:
+                s = settings
+
+            # Default backup_file name
+            if not backup_file or backup_file == 'default':
+                core_id = s.core_tag or s.core_commit
+                start_str = datetime.datetime.utcnow().strftime('%Y-%m-%dT%H-%M-%S')
+                assert core_id, "No commit tag or commit id found for odoo core at %s" % s.instance_core_dir
+                backup_name = s.instance + '__' + start_str + '__' + core_id + '.zip'
+                self.backup_file = pj(instance_dir, 'update', backup_name)
+                _log.info("No backup file name specified. Using default name and location: %s" % self.backup_file)
+
+            self.odoo_db_name = s.db_name
+            self.odoo_db_url = s.db_url
+            self.odoo_host = s.instance_local_url
+            self.odoo_data_dir = s.data_dir
+            self.odoo_master_pwd = s.master_password
+
+
+def backup(instance_dir, env=None, backup_file='', mode='manual', db_url=None, data_dir=None,
+           log_file='', cmd_args=None, settings=None,  timeout=60*60*4):
     """
     Backup an FS-Online instance
 
     HINT: manual mode seems to be much faster and less cpu intensive therefore it is the default
 
     :param timeout: (int) timeout in seconds
-    :param settings: instance settings object
+    :param settings: odoo instance settings object
     :param mode: (str) backup mode
     :param instance_dir: (str) Directory of the instance to backup
+                               or just the instance name for manual backups on remote servers
     :param backup_file: (str) Full Path and file name
     :param cmd_args: (list) with cmd options
     :param log_file: (str) Full Path and file name
     :return: (str) 'backup_file' if backup worked or (boolean) 'False' if backup failed
     """
     cmd_args = list() if not cmd_args else cmd_args
-    mode_allowed = ('all', 'http', 'manual')
+    mode_allowed = ('manual', 'odoo')
     assert mode in mode_allowed, "mode must be one of %s" % str(mode_allowed)
-    
-    instance_dir = os.path.abspath(instance_dir)
-    instance = os.path.basename(instance_dir)
-    _log.info('----------------------------------------')
-    _log.info('BACKUP instance %s' % instance)
-    _log.info('----------------------------------------')
 
-    # Get odoo settings
-    _log.info("Get instance settings from %s" % instance_dir)
-    s = settings if settings else Settings(instance_dir, startup_args=cmd_args, log_file=log_file)
+    # COMPUTE SETTINGS FOR BACKUP
+    # ---------------------------
+    bse = BackupSettings(instance_dir, env=env, backup_file=backup_file, mode=mode, db_url=db_url,
+                         data_dir=data_dir, cmd_args=cmd_args, settings=settings, log_file=log_file)
 
-    # Default backup file name
-    if not backup_file or backup_file == 'default':
-        core_id = s.core_tag or s.core_commit
-        start_str = datetime.datetime.utcnow().strftime('%Y-%m-%dT%H-%M-%S')
-        assert core_id, "No commit tag or commit id found for odoo core at %s" % s.instance_core_dir
-        backup_name = instance + '__' + start_str + '__' + core_id + '.zip'
-        backup_file = pj(instance_dir, 'update', backup_name)
-        _log.info("No backup file name specified. Using default name and location: %s" % backup_file)
+    # START BACKUP
+    # ------------
+    _log.info('--------------------------------------------------------------------------------')
+    _log.info('BACKUP (db: %s, instance %s)' % (bse.odoo_db_name, bse.instance_dir))
+    _log.info('--------------------------------------------------------------------------------')
 
-    # Clean backup_file path
-    backup_file = os.path.abspath(backup_file)
-    backup_archive = None
-
-    # Backup via http post request (= streaming by odoo)
-    # --------------------------------------------------
-    if mode in ('all', 'http') and not backup_archive:
-        _log.info("Try regular backup via http connection to odoo")
+    # Backup via odoo post request (= streaming by odoo)
+    if mode == 'odoo':
+        _log.info("Try odoo backup via odoo service")
         try:
-            backup_archive = tools_odoo.odoo_backup(s.db_name, backup_file, host=s.instance_local_url,
-                                                    master_pwd=s.master_password, timeout=timeout)
+            backup_archive = tools_odoo.odoo_backup(bse.odoo_db_name,
+                                                    bse.backup_file,
+                                                    host=bse.odoo_host,
+                                                    master_pwd=bse.odoo_master_pwd,
+                                                    timeout=timeout)
         except Exception as e:
-            backup_archive = False
-            _log.warning("Http streaming backup failed! %s" % repr(e))
-            if mode == 'http':
-                raise e
-
-    # Manual backup via file copy and pg_dump
-    # ---------------------------------------
-    if mode in ('all', 'manual') and not backup_archive:
-        _log.info("Try manual backup via database url and data_dir copy")
-        try:
-            backup_archive = tools_odoo.odoo_backup_manual(db_url=s.db_url, data_dir=s.data_dir, backup_file=backup_file,
-                                                           timeout=timeout)
-        except Exception as e:
-            msg = "Manual backup failed! %s" % repr(e)
-            _log.error(msg)
+            _log.warning("Odoo backup failed! %s" % repr(e))
             raise e
 
-    assert backup_archive, "BACKUP OF INSTANCE %s TO %s FAILED!" % (s.instance, backup_file)
+    # Manual backup via file copy and pg_dump
+    if mode == 'manual':
+        _log.info("Try manual odoo backup via database dump and data_dir copy")
+        try:
+            backup_archive = tools_odoo.odoo_backup_manual(db_url=bse.odoo_db_url,
+                                                           data_dir=bse.odoo_data_dir,
+                                                           backup_file=bse.backup_file,
+                                                           timeout=timeout)
+        except Exception as e:
+            _log.error("Manual odoo backup failed! %s" % repr(e))
+            raise e
+
+    assert backup_archive, "BACKUP OF INSTANCE %s TO %s FAILED!" % (bse.instance_dir, bse.backup_file)
 
     # _log result
-    _log.info("BACKUP OF INSTANCE %s TO %s DONE!" % (s.instance, backup_archive))
+    _log.info("BACKUP OF INSTANCE %s TO %s DONE!" % (bse.instance_dir, backup_archive))
 
     # Return 'path to backup file' or False
     return backup_archive
--- fsonline.py
+++ fsonline.py
@@ -25,6 +25,38 @@ import os
 from os.path import join as pj
 import sys
 import time
+import textwrap
+import logging
+
+# Initialize the logging
+# Get a handle to the root logger (or instantiate it the first and only time)
+# HINT: The root logger is a singleton so all calls to it will return the same object!
+#logging.basicConfig(format='%(asctime)s %(levelname)-8s %(module)-14s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+_log = logging.getLogger()
+_log.setLevel(logging.DEBUG)
+# Create a format object to be used in log handlers
+log_formatter = logging.Formatter(fmt='%(asctime)s %(levelname)-8s %(module)-14s %(message)s',
+                                  datefmt='%Y-%m-%d %H:%M:%S')
+# Log in GMT time (instead of localtime) to have the same time than odoo logging
+log_formatter.converter = time.gmtime
+log_sys_handler = logging.StreamHandler(sys.stdout)
+log_sys_handler.setFormatter(log_formatter)
+log_sys_handler.setLevel(logging.DEBUG)
+
+# Add the handler to the root logger
+_log.addHandler(log_sys_handler)
+
+
+# Redirect sys assertion outputs to the logger
+def excepthook(*eargs):
+    print "SYS excepthook() %s" % repr(eargs)
+    # Get the root logger and log to CRITICAL
+    logging.getLogger(__name__).critical('Uncaught exception:\n-------------------\n%s', eargs,
+                                         exc_info=True)
+
+
+sys.excepthook = excepthook
+
 
 # ATTENTION: segfault warning! backup > tools_odoo ... import requests
 #            Importing requests may lead to segmentation faults on odoo startup on the production servers!!!
@@ -38,141 +70,10 @@ import update
 import start
 
 
-import logging
-# Globally initialize the logging for this file
-# Get a handle to the root logger (or instantiate it the first and only time)
-# HINT: The root logger is a singleton so all calls to it will return the same object!
-_log = logging.getLogger()
-_log.setLevel(logging.DEBUG)
-# Create a format object to be used in log handlers
-log_formatter = logging.Formatter(fmt='%(asctime)s %(levelname)-8s %(module)-14s %(message)s',
-                                  datefmt='%Y-%m-%d %H:%M:%S')
-# Log in GMT time (instead of localtime) to have the same time than odoo logging
-log_formatter.converter = time.gmtime
-log_sys_handler = logging.StreamHandler(sys.stdout)
-log_sys_handler.setFormatter(log_formatter)
-log_sys_handler.setLevel(logging.DEBUG)
-# Add the handler to the root logger
-_log.addHandler(log_sys_handler)
-
-
-def excepthook(*eargs):
-    print "SYS excepthook() %s" % repr(eargs)
-    # Get the root logger and log to CRITICAL
-    logging.getLogger(__name__).critical('Uncaught exception:\n'
-                                         '-------------------\n', exc_info=eargs)
-
-
-# Redirect sys assertion outputs to the logger
-sys.excepthook = excepthook
-
-
-# ------------------------------------
-# COMMAND PARSER DEFAULT FUNCTION CALL
-# ------------------------------------
-def fs_online():
-    # BACKUP
-    if known_args.backup:
-        result = backup.backup(known_args.instance_dir,
-                               backup_file=known_args.backup,
-                               cmd_args=unknown_args, log_file=known_args.log_file)
-        if result:
-            exit(0)
-        else:
-            exit(100)
-
-    # RESTORE
-    if known_args.restore:
-        restore.restore(known_args.instance_dir,
-                        backup_zip_file=known_args.restore,
-                        cmd_args=unknown_args, log_file=known_args.log_file)
-        exit(0)
-
-    # DEVELOPMENT_RESTORE
-    if known_args.development_restore:
-        answer_dev_restore = ''
-        while answer_dev_restore not in ('Yes', 'No'):
-            answer_dev_restore = raw_input("The database for instance '%s' will be dropped without a backup!\n"
-                                           "Are you sure you want to do a development-restore of '%s'?\n"
-                                           "[Yes/No]: "
-                                           "" % (known_args.instance_dir, known_args.development_restore))
-            if answer_dev_restore == 'Yes':
-                restore.restore(known_args.instance_dir,
-                                backup_zip_file=known_args.development_restore,
-                                cmd_args=unknown_args, log_file=known_args.log_file,
-                                backup_before_drop=False,
-                                start_after_restore=False,
-                                development_mode=True)
-        exit(0)
-
-    # UPDATE
-    if known_args.update:
-        update.update(known_args.instance_dir,
-                      update_branch=known_args.update,
-                      cmd_args=unknown_args, log_file=known_args.log_file)
-        exit(0)
-
-    # START
-    start.start(known_args.instance_dir, cmd_args=unknown_args, log_file=known_args.log_file)
-
-
-# ----------------------------
-# COMMAND PARSER
-# ----------------------------
-# Create a new argument-parser object
-parser = argparse.ArgumentParser()
-
-# Add Arguments
-parser.add_argument('instance_dir', help="Instance directory")
-parser.add_argument('--log_file',
-                    metavar='Not set for STDOUT or /path/log_file.log',
-                    help='Log file for this script and odoo. Emtpy for stdout. '
-                         'Add "--logfile=/path/too/odoo.log" or add "logfile = /path/too/odoo.log" to server.conf '
-                         'if you want a separate log-file for the odoo log messages.')
-parser.add_argument('--verbose', help='Log Level',
-                    choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'],
-                    default='INFO')
-
-# Additional modes for this script
-parser.add_argument('--backup',
-                    nargs='?',          # ? means 0-or-1 arguments
-                    const='default',    # sets this default when there are 0 arguments
-                    type=str,           # converts the argument to string
-                    help='Create a backup at the given file name! Will backup to default location '
-                         '/[instance_dir]/update/[backupname.zip] if no backupfile is given!')
-
-parser.add_argument('--restore',
-                    metavar='/path/to/backup/backup.zip',
-                    help='Restore from backup zip or from folder')
-
-parser.add_argument('--development_restore',
-                    metavar='/path/to/backup/backup.zip',
-                    help='Restore from backup zip or from folder')
-
-parser.add_argument('--update',
-                    nargs='?',      # ? means 0-or-1 arguments
-                    const='o8',     # sets this default when there are 0 arguments
-                    type=str,       # converts the argument to string
-                    help='Update to branch or commit of the instance repository in github! Defaults to: "o8"')
-
-# Set a default function to be called after the initialization of the parser object
-parser.set_defaults(func=fs_online)
-
-
-# --------------------
-# START
-# --------------------
-if __name__ == "__main__":
-    # Globally available data stores (vars):
-    # -------------------------------------------------------------------------------
-    # known_args        (Namespace)                 CMD settings known to command parser
-    # unknown_args      (list)                      CMD settings not set in command parser
-    # log               (logging instance)          Globally used logger __main__
-    # -------------------------------------------------------------------------------
-
-    # Get the command line arguments
-    known_args, unknown_args = parser.parse_known_args()
-
+# -----------------------------
+# COMMAND PARSER FUNCTION CALLS
+# -----------------------------
+def fson_start(known_args=None, unknown_args=None):
     # Make sure the instance path exits
     assert os.path.exists(known_args.instance_dir), 'Instance directory not found at %s!' % known_args.instance_dir
 
@@ -180,8 +81,202 @@ if __name__ == "__main__":
     instance_ini_file = pj(known_args.instance_dir, 'instance.ini')
     assert os.path.isfile(instance_ini_file), 'File instance.ini not found at %s!' % instance_ini_file
 
-    # START LOGGING
-    # -------------
+    start.start(known_args.instance_dir, cmd_args=unknown_args, log_file=known_args.log_file)
+
+
+def fson_backup(known_args=None, unknown_args=None):
+    assert known_args.backup, "'backup' argument missing in known_args"
+    result = backup.backup(known_args.instance_dir,
+                           env=known_args.env,
+                           backup_file=known_args.backup,
+                           mode=known_args.backup_mode,
+                           db_url=known_args.db_url,
+                           data_dir=known_args.data_dir,
+                           cmd_args=unknown_args,
+                           log_file=known_args.log_file)
+    if result:
+        exit(0)
+    else:
+        exit(100)
+
+
+def fson_restore(known_args=None, unknown_args=None):
+    if known_args.restore:
+        restore.restore(known_args.instance_dir,
+                        backup_zip_file=known_args.restore,
+                        development_mode=bool(known_args.env == 'development'),
+                        cmd_args=unknown_args,
+                        log_file=known_args.log_file)
+        exit(0)
+
+
+def fson_update(known_args=None, unknown_args=None):
+    if known_args.update:
+        update.update(known_args.instance_dir,
+                      update_branch=known_args.update,
+                      cmd_args=unknown_args,
+                      log_file=known_args.log_file)
+        exit(0)
+
+# ----------------------------
+# COMMAND PARSER
+# ----------------------------
+# Support the "old" commands format:
+#    fsonline.py /path/to/instance --backup=/path/to/backup/file.zip
+#    fsonline.py /path/to/instance --backup /path/to/backup/file.zip
+#
+# We redirect the parsing of the unknown_args to the 'parser_for_subcommands' for known subcommands
+# This is kind of a hack to keep the old interface of fs-online.py. It is necessary because argparse is not
+# supporting "optional" subcommands. Check the following URL for more information on this:
+# https://stackoverflow.com/questions/46667843/how-to-set-a-default-subparser-using-argparse-module-with-python-2-7
+
+
+# THE MAIN PARSER FOR A REGULAR ODOO START
+# ----------------------------------------
+# TODO: Add some help/hints to the parser_main for the available modes/subcommands and how to get help!
+parser_main = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,
+                                      epilog=textwrap.dedent(
+"""
+---
+Start, backup, restore or update an fs-online odoo instance.
+
+Available subcommands are: 'backup', 'restore' and 'update'
+You can get help for the subcommands like this: fsonline.py does_not_matter --backup -h
+
+Example usages:
+    
+    fsonline.py /opt/online/dadi
+    fsonline.py /opt/online/dadi -l /log/mylog.log
+    fsonline.py /opt/online/dadi -l /log/mylog.log -u all --stop-after-init
+    
+    fsonline.py /opt/online/dadi --backup
+    fsonline.py /opt/online/dadi --backup --mode=odoo
+    fsonline.py dadi --backup=/path/to/backup_file.zip --db_url=*** --data_dir=/path/to/odoo/data_dir
+    
+    fsonline.py /opt/online/dadi --restore=/path/to/backup_file.zip
+    fsonline.py /opt/online/dadi --env=development --restore=/path/to/backup_file.zip
+    
+    fsonline.py /opt/online/dadi --update=o8r356
+    fsonline.py /opt/online/dadi --update=86a7ae75da902b3f46a0f808b348a38aed31c622
+---
+""")
+                                      )
+parser_main.set_defaults(func=fson_start)
+parser_main.add_argument('instance_dir',
+                    help="Path to instance directory or instance name for manual backups.")
+parser_main.add_argument('-l', '--log_file',
+                    metavar='/path/to/log_file.log (Will log to STDOUT if not set)',
+                    help='Add "logfile = /path/too/odoo.log" to the odoo server.conf '
+                         'if you want a separate log file for the odoo log messages.')
+parser_main.add_argument('-v', '--verbose',
+                    help='Log Level',
+                    choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'],
+                    default='INFO')
+parser_main.add_argument('-e', '--env',
+                    nargs='?',                  # ? means 0-or-1 argument values
+                    default='production',       # Set this value when the argument is missing completely
+                    const='production',         # Set this value when there is no value for the argument
+                    choices=['production', 'development'],
+                    help='In the development environment there will be additional cleanups for backup and restore! '
+                         'NEVER use "development" on a production instance!')
+
+# CREATE A SECOND PARSER FOR THE SUBCOMMANDS
+# ------------------------------------------
+parser_for_subcommands = argparse.ArgumentParser()
+subparsers = parser_for_subcommands.add_subparsers(dest="subcommand_name", help='Extra Modes')
+
+# BACKUP
+subparser_backup = subparsers.add_parser("backup",
+                                         help="Backup Instance")
+subparser_backup.set_defaults(func=fson_backup)
+subparser_backup.add_argument('--backup',
+                              nargs='?',            # ? means 0-or-1 argument values
+                              const='default',      # Set this default when there is no value for the argument
+                              metavar='/path/to/backup/file.zip',
+                              type=str,             # converts the argument to string
+                              help='Will backup to default location /[instance_dir]/update/[backupname.zip] if no '
+                                   'path is given!')
+subparser_backup.add_argument('--backup_mode',
+                              nargs='?',            # ? means 0-or-1 argument values
+                              default='manual',     # Set this value when the argument is missing completely
+                              choices=['manual', 'odoo'],
+                              help='Mode manual will use file copy and pg_dump for the backup.\n'
+                                   'Mode odoo will use odoo for the backup.\n'
+                                   'HINT: The options db_url and data_dir only apply in manual mode!')
+subparser_backup.add_argument('--db_url',
+                              nargs='?',            # ? means 0-or-1 argument values
+                              default='',
+                              type=str,             # converts the argument to string
+                              help='The postgresql database url in the format xxx. '
+                                   'Only used for manual backups!')
+subparser_backup.add_argument('--data_dir',
+                              nargs='?',            # ? means 0-or-1 argument values
+                              default='',           # Set this value when the argument is missing completely
+                              type=str,             # converts the argument to string
+                              help='The odoo data_dir location. e.g. /opt/online/dadi/data_dir. '
+                                   'Only used for manual backups')
+
+
+# RESTORE
+subparser_restore = subparsers.add_parser("restore",
+                                      help="Restore Instance")
+subparser_restore.set_defaults(func=fson_restore)
+subparser_restore.add_argument('--restore',
+                               required=True,
+                               type=str,
+                               metavar='/path/to/backup/file.zip',
+                               help='Restore from backup zip or from folder')
+
+# UPDATE
+subparser_update = subparsers.add_parser("update",
+                                      help="Update Instance")
+subparser_update.set_defaults(func=fson_update)
+subparser_update.add_argument('--update',
+                              nargs='?',    # ? means 0-or-1 argument values
+                              const='o8',   # Set this default when there is no value for the argument
+                              type=str,     # converts the argument to string
+                              help='Update to branch or commit of the instance repository in github! Defaults to: "o8"')
+
+
+# --------------------
+# START
+# --------------------
+if __name__ == "__main__":
+    # Globally available variables:
+    # -------------------------------------------------------------------------------
+    # known_args        (Namespace)                 CMD settings known to command parser
+    # unknown_args      (list)                      CMD settings not set in command parser
+    # _log              (logging instance)          Globally used logger __main__
+    # -------------------------------------------------------------------------------
+
+    # Preparations to redirect -h or --help to the correct parser
+    cmd_args_no_help = [s for s in sys.argv[1:] if s not in ['-h', '--help']]
+    help = bool(len(cmd_args_no_help) < len(sys.argv)-1)
+
+    # Parse the arguments of the 'parser_main'
+    known_args, unknown_args = parser_main.parse_known_args(cmd_args_no_help)
+
+    # Make sure none or exactly one 'known subcommand' is given
+    known_subcommands = ['backup', 'restore', 'update']
+    subcommand_count = 0
+    for unknown_arg in unknown_args:
+        if any(unknown_arg.lstrip('--').split('=')[0] == known_sc for known_sc in known_subcommands):
+            subcommand_count += 1
+    assert subcommand_count <= 1, "Multiple subcommands are not supported!"
+
+    # Parse subcommand arguments with the special parser 'parser_for_subcommands' AND redirect -h to the correct parser
+    subcommand = unknown_args[0].split('=')[0].lstrip('--') if len(unknown_args) > 0 else None
+    if subcommand and subcommand in known_subcommands:
+        unknown_args.insert(0, subcommand)
+        if help:
+            parser_for_subcommands.parse_known_args([subcommand, '-h'], namespace=known_args)
+            exit(0)
+        else:
+            known_args, unknown_args = parser_for_subcommands.parse_known_args(unknown_args, namespace=known_args)
+
+    if help:
+        known_args, unknown_args = parser_main.parse_known_args()
+        exit(0)
 
     # Set the log Level
     log_level = logging.getLevelName(known_args.verbose)
@@ -211,7 +306,5 @@ if __name__ == "__main__":
     _log.info('known_args: %s' % known_args)
     _log.info('unknown_args: %s' % str(unknown_args))
 
-    # START fs_online()
-    # -----------------
-    # HINT: See "parser.set_defaults(func=fs_online)" above
-    known_args.func()
+    # START THE DEFAULT PARSER FUNCTION
+    known_args.func(known_args=known_args, unknown_args=unknown_args)
